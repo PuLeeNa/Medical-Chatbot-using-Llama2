45d4e3a372362dedc5ecb637a40ad6cff20a79d6
Your Name <wickramasooriyabgpv.22@uom.lk>
2025-12-01 23:27:49 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical_chatbot",
+    version="0.0.0",
+    author="Pu Lee Na",
+    author_email="pulina28062001@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
